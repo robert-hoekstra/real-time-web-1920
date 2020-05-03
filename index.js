c2b4efe1e41a78ff268009f0c5961551b369554d
Robert Hoekstra <robert.hoekstra@hva.nl>
2020-05-03 23:20:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,10 @@ var express = require('express'),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server)
 
-server.listen(process.env.PORT || 4000);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
 
 var clients = io.clients;
 
